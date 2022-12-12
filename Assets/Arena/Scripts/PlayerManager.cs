5c5ec69717883a84da9d293f8aaa7cabf8fff8d3
oscarbaltasar <oscarbaltasar@hotmail.com>
2022-12-12 13:42:12 +0100

    Añadido effecto portal, reloj y gameover a Arena

--- Assets/Arena/Scripts/PlayerManager.cs
+++ Assets/Arena/Scripts/PlayerManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
 
     public float vida;
     public float puntuacion;
+    public TextMeshProUGUI roundText;
 
     private float vidaInicial;
 
     void Start()
     {
         vidaInicial = vida;
+    }
+
+    private void Update()
+    {
+        if(vida <= 0)
+        {
+            SceneManager.LoadScene("Hub", LoadSceneMode.Single);
+        }
+        roundText.text = ((int)vida).ToString();
     }
 
     public void ResetHealth()
